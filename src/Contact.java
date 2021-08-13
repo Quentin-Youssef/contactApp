b98556a8b1f9246a566d0af888675854611e5d1a
Quentin Hewitt <quentin.l.hewitt@gmail.com>
2021-08-13 15:09:14 -0500

    added contactlist

---
+++ src/Contact.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Contact {
+
+    public static void contactApp(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What action do you want to perform1. View contacts.\n" +
+                "2. Add a new contact.\n" +
+                "3. Search a contact by name.\n" +
+                "4. Delete an existing contact.\n" +
+                "5. Exit.\n" +
+                "Enter an option (1, 2, 3, 4 or 5):");
+    }
+}
